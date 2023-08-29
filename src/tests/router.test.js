c20e0b4759f89380ad3cec6d2dc3d726ff06308b
MrDrog <fireman3085@mail.ru>
2023-08-29 18:22:47 +0300

    fix: fixed routing tests with jest

--- src/tests/router.test.js
+++ src/tests/router.test.js
@@ -1,17 +1,18 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from '../App'
 
 
 describe('Router test', () => {
 
     test('Test for Help route', () => {
+
         render(<App/>)
         const helpLink = screen.getByText('Help')
-        // userEvent.click(helpLink)
+        userEvent.click(helpLink)
         expect(screen.getByText(`Today's videos:`)).toBeInTheDocument()
-        // expect(screen.getByText('Contacts')).toBeInTheDocument()
-        // userEvent.click(helpLink)
-        // expect(screen.getByText(`Today's videos:`)).toBeInTheDocument()
+        userEvent.click(helpLink)
+        expect(screen.getByText('Contacts')).toBeInTheDocument()
     })
     
 })
